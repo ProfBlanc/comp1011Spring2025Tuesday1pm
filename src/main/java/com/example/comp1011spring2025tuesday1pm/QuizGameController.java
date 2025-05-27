20fd18ed9cb613f4aa2096ad61a1cdbee95a680c
Ben <ben.blanc@georgebrown.ca>
2025-05-27 15:56:27 -0400

    Class Code May 27, 2025 @ 15h55

--- src/main/java/com/example/comp1011spring2025tuesday1pm/QuizGameController.java
+++ src/main/java/com/example/comp1011spring2025tuesday1pm/QuizGameController.java
@@ -4,20 +4,50 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.util.List;
 
 public class QuizGameController {
 
     @FXML
     private Button btnSubmit;
 
+
     @FXML
-    private ComboBox<?> cbNickname;
+    private ComboBox<String> cbNickname;
 
     @FXML
     private ImageView imgAvatar;
 
     @FXML
-    private Spinner<?> imgSpinner;
+    private Spinner<Integer> imgSpinner;
+
+
+    @FXML
+    public void initialize(){
+
+        cbNickname.getItems().addAll(Participant.getAllNames());
+        cbNickname.getSelectionModel().selectFirst();
+
+       List<Image> images =  Participant.getAllAvatars().stream()
+                .map(v -> new Image(getClass().getResourceAsStream("avatars/" + v)))
+               .toList();
+
+        imgAvatar.setImage(images.get(0));
+
+        imgSpinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, images.size() - 1, 0));
+
+
+        imgSpinner.valueProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    imgAvatar.setImage(images.get(newValue));
+                }
+        );
+
+
+    }
 
 }
